7e15f060c1d12ad8438313675ff58c325cdc7d9f
Midry <115441271+midry3125@users.noreply.github.com>
2025-04-09 19:14:59 +0900

    修正

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import datetime
 import os
+import sys
 
 import discord
 from discord.ext import commands
-
-TOKEN = os.environ["DISCORD_API_KEY"] # APIキー
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -15,23 +14,44 @@ async def on_ready():
     pass
 
 @bot.command()
-async def get(ctx):
-    info = get_menu_info() # メニュー表取得
-    today = str(datetime.datetime.now().day)
-    for i in info:
-        if today in i[0][1:]:
-            idx = 1
-            while True:
-                if today == i[0][idx].split("日")[0]:
-                    break
-                idx += 1
-            msg = f"今日の日替わりメニュー\n"
-            for m in i[1:]:
-                msg += f"{m[0]}: {m[idx]}\n"
-            break
+async def today(ctx):
+    msg = make_message()
     await ctx.send(msg)
 
+@bot.command()
+async def week(ctx):
+    msg = make_message(True)
+    await ctx.send(msg)
+
+def make_message(week=False):
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        info = eval(f.read())
+    today =  "4" or str(datetime.datetime.now().day)
+    for i in info:
+        for n, d in enumerate(i[0][1:]):
+            if today == d.split("日")[0].strip():
+                idx = n + 1
+                break
+        else:
+            continue
+        if week:
+            msg = ""
+            for n in range(len(i)-1):
+                msg += f"{i[0][n+1]}のメニュー\n"
+                for m in range(len(i[0])-1):
+                    msg += "{}: {}\n".format(i[m+1][0], i[m+1][n+1].replace("\n", ", "))
+                msg += "\n"
+            return msg
+                
+        else:
+            msg = f"今日の日替わりメニュー\n"
+            for m in i[1:]:
+                msg += "{}: {}\n".format(m[0], m[idx].replace("\n", ", "))
+            return msg
+    return "まだ情報がありません"
+
 def main():
+    TOKEN = os.environ["DISCORD_API_KEY"] # APIキー
     bot.run(TOKEN)
 
 if __name__ == "__main__":
